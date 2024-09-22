d6f991421347fa4bb80ebda9c79629d48f0fc298
albinofox9 <albinofox9@gmail.com>
2024-09-23 08:44:01 +1000

    square update

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 const HomePage = () => {
   return (
     <div>
-      <div className="absolute z-[-1] bg-c2 w-[50%] h-[110vh] max-h-[110%] overflow-y-hidden top-0 bot-0 rounded-tr-[475px]"></div>
+      <div className="absolute z-[-1] bg-c2 w-[50%] h-[115vh] max-h-[115%] overflow-y-hidden top-0 bot-0 rounded-tr-[475px]"></div>
       <div className="flex pd-default items-center justify-between">
         <div className="flex flex-col w-[720px]">
           <div className="font-light big-text pb-[62px]">
