ce8cc256cab3a24669292565a5fce474b9a5e6f8
albinofox9 <albinofox9@gmail.com>
2024-09-22 21:42:23 +1000

    some adjustments

--- app/(home)/profile/page.tsx
+++ app/(home)/profile/page.tsx
@@ -5,9 +5,13 @@ import InputField from "../_components/input";
 const ProfilePage = () => {
   return (
     <div className="flex pd-default justify-around gap-[34px] pt-[25px]">
-      <div className="flex flex-col flex-shrink-0">
+      <div className="flex flex-col flex-shrink-0 w-[45%]">
         <div className="flex flex-row gap-5 items-center">
-          <Button size="fix" variant="icon" className="w-[150px] h-[150px]" />
+          <Button
+            size="fix"
+            variant="icon"
+            className="w-[150px] h-[150px] flex-shrink-0"
+          />
           <div className="flex flex-col">
             <div className="big-text">Really long name (tm)</div>
             <div className="small-text">example@gmail.com</div>
@@ -16,11 +20,7 @@ const ProfilePage = () => {
         <div>Тип аккаунта: Студент</div>
       </div>
       <div className="flex flex-col basis-1/2 flex-shrink-0 max-w-[1200px]">
-        <Form
-          btnTitle="Сохранить изменения"
-          btnFill
-          className="w-[696px] pb-[100px]"
-        >
+        <Form btnTitle="Сохранить изменения" btnFill className="pb-[100px]">
           <div className="pb-[20px]">Данные профиля:</div>
           <InputField title="Имя" isImportant />
           <InputField title="Фамилия" />
