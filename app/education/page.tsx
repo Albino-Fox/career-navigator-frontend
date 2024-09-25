b6389bd69b983a233e8cde24a973bfb1e358502f
albinofox9 <albinofox9@gmail.com>
2024-09-26 04:24:16 +1000

    work tasks fetch on students education page

--- app/education/page.tsx
+++ app/education/page.tsx
@@ -11,8 +11,12 @@ import CardUniversity from "./_components/cardUniversity";
 import CardSkill from "./_components/cardSkill";
 import CardWork from "./_components/cardWork";
 import ListCardWork from "./_components/listCardWork";
+import { SkillName } from "@/types/card";
 
-const EducationPage = () => {
+const EducationPage = async () => {
+  const response = await fetch("http://127.0.0.1:3001/api/vacancies/get");
+  const vacancies = await response.json();
+
   return (
     <div className="flex justify-center w-[100%] h-fit bg-c5 pb-[75px]">
       <div className="flex flex-col gap-[10px] items-center w-[836px]">
@@ -39,46 +43,29 @@ const EducationPage = () => {
                       Рабочие задачи
                     </DialogTitle>
                     <DialogDescription>
-                      <ListCardWork
-                        title="Сайт для продажи одежды"
-                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                        skill={{
-                          difficulty: 2,
-                          title: "Frontend Developer",
-                        }}
-                      >
-                        <Button>Взять в фокус</Button>
-                      </ListCardWork>
-                      <ListCardWork
-                        title="Сайт для продажи одежды"
-                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                        skill={{
-                          difficulty: 2,
-                          title: "Frontend Developer",
-                        }}
-                      >
-                        <Button variant="destructive">Снять фокус</Button>
-                      </ListCardWork>
-                      <ListCardWork
-                        title="Сайт для продажи одежды"
-                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                        skill={{
-                          difficulty: 2,
-                          title: "Frontend Developer",
-                        }}
-                      >
-                        <Button>Взять в фокус</Button>
-                      </ListCardWork>
-                      <ListCardWork
-                        title="Сайт для продажи одежды"
-                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                        skill={{
-                          difficulty: 2,
-                          title: "Frontend Developer",
-                        }}
-                      >
-                        <Button>Взять в фокус</Button>
-                      </ListCardWork>
+                      {vacancies.map(
+                        (vacancy: {
+                          title: string;
+                          description: string;
+                          skillId: number;
+                          id: number;
+                          level: number;
+                          skillTitle: SkillName;
+                        }) => (
+                          <ListCardWork
+                            key={vacancy.id}
+                            title={vacancy.title}
+                            id={vacancy.id}
+                            description={vacancy.description}
+                            skill={{
+                              difficulty: vacancy.level,
+                              title: vacancy.skillTitle,
+                            }}
+                          >
+                            <Button>Взять в фокус</Button>
+                          </ListCardWork>
+                        ),
+                      )}
                     </DialogDescription>
                   </DialogHeader>
                 </DialogContent>
